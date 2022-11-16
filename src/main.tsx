fa73de942f2aef381e3b450d9f67c55cbbdd7cc7
Nguyễn Thanh An <annt22@vng.com.vn>
2022-11-16 12:51:05 +0700

    remove unused resources

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
+import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import './index.css';
 
 ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 );
