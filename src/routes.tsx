5ef9bc6ab56e13f0626cb15c53b60f24e4f55c35
Toan <nguyenquoctoan1230@gmail.com>
2022-11-18 14:27:54 +0700

    404 not found page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import RegisterPage from './components/authentication/register';
+import NotFoundPage from './components/errorPage/notFound';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <div>Home</div>,
+    errorElement: <NotFoundPage />,
   },
   {
     path: '/register',
