d3ddf8d64a6f3a849dde1e04526788b351902e91
Toan <nguyenquoctoan1230@gmail.com>
2022-11-28 11:08:44 +0700

    add loading and empty state in group list

--- src/pages/groups/list/index.tsx
+++ src/pages/groups/list/index.tsx
@@ -1,5 +1,5 @@
 import {
-  Container, Grid, Group, Card, Image, Stack, Text, Pagination, createStyles, Tooltip,
+  Container, Grid, Group, Card, Image, Stack, Text, Pagination, createStyles, Tooltip, Loader, Center,
 } from '@mantine/core';
 import {
   useState, useEffect, useCallback, useMemo,
@@ -29,14 +29,26 @@ const useStyles = createStyles(() => ({
   },
 }));
 
+function EmptyContent() {
+  return (
+    <Container>
+      <Text align="center" color="blue" weight={600} size="xl" my="xl">
+        Create or join a group to start
+      </Text>
+    </Container>
+  );
+}
+
 export default function GroupsPage() {
   const { classes } = useStyles();
   const [dataSource, setDataSource] = useState<GroupType[]>([]);
   const [activePage, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [isLoading, setLoading] = useState(false);
 
   const fetchData = useCallback(async () => {
     try {
+      setLoading(true);
       const { data: response } = await groupApi.getMyGroups();
 
       setDataSource(response.data);
@@ -46,6 +58,8 @@ export default function GroupsPage() {
         notificationManager.showFail('', error.response?.data.message);
       }
     }
+
+    setLoading(false);
   }, []);
 
   useEffect(() => {
@@ -62,44 +76,63 @@ export default function GroupsPage() {
   return (
     <Container size="lg">
       <Header fetchData={fetchData} />
-      <Grid>
-        {currentDataSource.map((group, index) => (
-          <Grid.Col key={index} span={3}>
-            <Card
-              component={Link}
-              // eslint-disable-next-line no-underscore-dangle
-              to={`/group/${group._id}`}
-              shadow="sm"
-              p="lg"
-              radius="md"
-              h={333}
-              withBorder
-            >
-              <Card.Section>
-                <Image
-                  src="https://images.unsplash.com/photo-1503676260728-1c00da094a0b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1122&q=80"
-                  height={160}
-                  alt="Cover image"
-                />
-              </Card.Section>
+      {
+        // eslint-disable-next-line no-nested-ternary
+        isLoading
+          ? (
+            <Center>
+              <Loader />
+            </Center>
+          )
+          : (
+            currentDataSource.length > 0
+              ? (
+                <>
+                  <Grid>
+                    {
+                      currentDataSource.map((group, index) => (
+                        <Grid.Col key={index} span={3}>
+                          <Card
+                            component={Link}
+                            // eslint-disable-next-line no-underscore-dangle
+                            to={`/group/${group._id}`}
+                            shadow="sm"
+                            p="lg"
+                            radius="md"
+                            h={333}
+                            withBorder
+                          >
+                            <Card.Section>
+                              <Image
+                                src="https://images.unsplash.com/photo-1503676260728-1c00da094a0b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1122&q=80"
+                                height={160}
+                                alt="Cover image"
+                              />
+                            </Card.Section>
 
-              <Stack mt="md" mb="xs" spacing="xs">
-                <Tooltip label={group.name} position="top-start">
-                  <Text weight={600} className={classes.lineClamp1}>{group.name}</Text>
-                </Tooltip>
-                <Text className={classes.lineClamp1}>{group.userCreated.name}</Text>
-              </Stack>
+                            <Stack mt="md" mb="xs" spacing="xs">
+                              <Tooltip label={group.name} position="top-start">
+                                <Text weight={600} className={classes.lineClamp1}>{group.name}</Text>
+                              </Tooltip>
+                              <Text className={classes.lineClamp1}>{group.userCreated.name}</Text>
+                            </Stack>
 
-              <Text size="sm" color="dimmed" className={classes.lineClamp3}>
-                {group.description}
-              </Text>
-            </Card>
-          </Grid.Col>
-        ))}
-      </Grid>
-      <Group position="right" mt="lg">
-        <Pagination page={activePage} onChange={setPage} total={totalPages} />
-      </Group>
+                            <Text size="sm" color="dimmed" className={classes.lineClamp3}>
+                              {group.description}
+                            </Text>
+                          </Card>
+                        </Grid.Col>
+                      ))
+                    }
+                  </Grid>
+                  <Group position="right" mt="lg">
+                    <Pagination page={activePage} onChange={setPage} total={totalPages} />
+                  </Group>
+                </>
+              )
+              : <EmptyContent />
+          )
+      }
     </Container>
   );
 }
